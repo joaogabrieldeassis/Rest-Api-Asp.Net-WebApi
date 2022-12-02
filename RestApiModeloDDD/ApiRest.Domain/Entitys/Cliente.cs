38e3a2e01f17a6e8641f7d3abcf6bcae1610b5e7
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-12-02 07:46:28 -0300

    Criando os objetos

---
+++ RestApiModeloDDD/ApiRest.Domain/Entitys/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiRest.Domain.Entitys
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataDeCadastro { get; set; }
+        public bool IsAtivo { get; set; }
+    }
+}
---
+++ RestApiModeloDDD/ApiRest.Domain/Entitys/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiRest.Domain.Entitys
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public bool IsDisponivel{ get; set; }
+    }
+}
