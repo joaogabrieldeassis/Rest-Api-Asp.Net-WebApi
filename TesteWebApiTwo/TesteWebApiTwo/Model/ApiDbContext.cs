5289917eefe057702d7ef504ba4727e3f624834d
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-12-09 06:58:50 -0300

    Iniciando mais um projeto

---
+++ TesteWebApiTwo/TesteWebApiTwo/Model/ApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TesteWebApiTwo.Model
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Fornecedor> Fornecedores{ get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost,1433;Database=MinhaApiRest;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ TesteWebApiTwo/TesteWebApiTwo/Model/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TesteWebApiTwo.Model
+{
+    public class Fornecedor
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(100,ErrorMessage = "O campo {0} precisa ter entre 2 e 1 caracterers",MinimumLength = 2)]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre 2 e 1 caracterers", MinimumLength = 2)]
+        public string Documento { get; set; }
+        public int TipoFornecedor { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
