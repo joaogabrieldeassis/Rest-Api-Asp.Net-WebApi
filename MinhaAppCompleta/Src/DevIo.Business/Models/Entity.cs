5084846bc8eb7711e897a256fff1bee11b1ac603
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-12-14 09:28:15 -0300

    Criando as entidades

---
+++ MinhaAppCompleta/Src/DevIo.Business/Models/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIo.Business.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ MinhaAppCompleta/Src/DevIo.Business/Models/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIo.Business.Models
+{
+    public class Fornecedor
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
